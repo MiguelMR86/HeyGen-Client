eeb7a3fa5fea69aff3af0647c68313bd551bb122
Miguel Ángel Medina Ramírez <migueldevinfo@gmail.com>
2025-07-25 12:55:59 +0100

    Heygen settings initial commit

---
+++ src/settings/api.py
@@ -0,0 +1,12 @@
+import os
+from typing import Literal
+
+API_KEY = os.getenv("HEYGEN_API_KEY")
+BASE_API_URL = "https://api.heygen.com"
+
+API_URLS = {
+    "v1": f"{BASE_API_URL}/v1",
+    "v2": f"{BASE_API_URL}/v2",
+}
+
+UPLOAD_URL = "https://upload.heygen.com/v1"
